6730b111ddda7326a79b5460b307f47aa5e538e8
MdOchiuddinMiah <65400422+MdOchiuddinMiah@users.noreply.github.com>
2020-05-16 23:10:02 +0600

    Update AppSurveyRepository.cs

--- Server/AppAuthentication/Repository/AppSurveyRepository.cs
+++ Server/AppAuthentication/Repository/AppSurveyRepository.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using AppAuthenticationModel.Models;
 using System;
 using System.Collections.Generic;
@@ -25,28 +25,32 @@ namespace AppAuthentication.Repository
             _configuration = configuration;
         }
 
-        public async Task<List<PostView>> Posts()
+        public async Task<List<PostView>> Posts(string searchValue, int page, int pageSize)
         {
-
+            int start = ((page - 1) * pageSize) + 1;
+            int end = page * pageSize;
             List<PostView> postViews = new List<PostView>();
-            postViews = await _context.Post.Include(y => y.Comment).Select(data => new PostView
-            {
-                Id = data.Id,
-                PostTitle = data.PostTitle,
-                PostedBy = data.PostedByNavigation.Name,
-                PostedTime = data.PostedTime,
-                CommentViews = data.Comment.Select(ndata => new CommentView
+            postViews = await _context.Post.Include(y => y.Comment).Where(x =>
+              ((searchValue == "0") ? true : x.PostTitle.Equals(searchValue)) &&
+              ((x.Comment.Where(z => z.Id >= start && z.Id <= end).Count() == 0) ? false : true))
+                .Select(data => new PostView
                 {
-                    Id = ndata.Id,
-                    PostId = ndata.PostId,
-                    CommentTitle = ndata.CommentTitle,
-                    CommentedBy = ndata.CommentedByNavigation.Name,
-                    CommentedTime = ndata.CommentedTime,
-                    CommentId = ndata.Id,
-                    CommentLike = ndata.UserFeedback.Where(y => y.IsLike).Count(),
-                    CommentDislike = ndata.UserFeedback.Where(y => y.IsDislike).Count()
-                }).ToList()
-            }).ToListAsync();
+                    Id = data.Id,
+                    PostTitle = data.PostTitle,
+                    PostedBy = data.PostedByNavigation.Name,
+                    PostedTime = data.PostedTime,
+                    CommentViews = data.Comment.Where(z => z.Id >= start && z.Id <= end).Select(ndata => new CommentView
+                    {
+                        Id = ndata.Id,
+                        PostId = ndata.PostId,
+                        CommentTitle = ndata.CommentTitle,
+                        CommentedBy = ndata.CommentedByNavigation.Name,
+                        CommentedTime = ndata.CommentedTime,
+                        CommentId = ndata.Id,
+                        CommentLike = ndata.UserFeedback.Where(y => y.IsLike).Count(),
+                        CommentDislike = ndata.UserFeedback.Where(y => y.IsDislike).Count()
+                    }).ToList()
+                }).ToListAsync();
 
             return postViews;
         }
@@ -68,10 +72,10 @@ namespace AppAuthentication.Repository
             }
             else
             {
-                if(feedback.IsLike== commentFeedback.IsLike || feedback.IsDislike== !commentFeedback.IsLike)
+                if (feedback.IsLike == commentFeedback.IsLike || feedback.IsDislike == !commentFeedback.IsLike)
                 {
-                    
-                    throw new Exception("You already "+ (commentFeedback.IsLike ? "liked":"disliked"));
+
+                    throw new Exception("You already " + (commentFeedback.IsLike ? "liked" : "disliked"));
                 }
 
                 feedback.IsLike = commentFeedback.IsLike;
